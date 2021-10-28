a4f81d1b78deba8ae53be9aa94d12cfe927e9579
Clint Ngo <clintngo@gmail.com>
2021-10-28 13:49:32 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 import PersonCard from './Components/PersonCard'
-var person = [
-  {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
-  {"firstName":"John", "lastName":"Smith","age":88,"hairColor":"Brown"},
-  {"firstName":"Millard", "lastName":"Fillmore","age":50,"hairColor":"Brown"},
-  {"firstName":"Maria", "lastName":"Smith","age":62,"hairColor":"Brown"}
-]
 
 
 function App() {
+  var person = [
+    {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
+    {"firstName":"John", "lastName":"Smith","age":88,"hairColor":"Brown"},
+    {"firstName":"Millard", "lastName":"Fillmore","age":50,"hairColor":"Brown"},
+    {"firstName":"Maria", "lastName":"Smith","age":62,"hairColor":"Brown"}
+  ]
   return (
     <div className="App">
-
         {person.map(person =>{
         return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
         })
